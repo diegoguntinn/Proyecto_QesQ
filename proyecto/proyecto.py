4889b49a781655779a4eff7194df2e2d5025ee04
Casal <javierlolcasal@gmail.com>
2024-12-05 13:08:56 +0100

    feat: :sparkles: fotos con X y primer intento del onclick

--- proyecto/proyecto.py
+++ proyecto/proyecto.py
@@ -1,95 +1,101 @@
-# import reflex as rx
-#i mport os
-#
-#class State(rx.State):
-#    """The app state."""
-#
-#
-#@rx.page(route="/", title="proyecto")
-#
-#def index() -> rx.Component:
-#    
-#    assets_dir = os.path.join(os.path.dirname(__file__), "../assets/")
-#    try:
-#        image_files = sorted([f for f in os.listdir(assets_dir) if f.endswith((".png", ".jpg", ".jpeg"))])
-#        print("Imágenes encontradas:", image_files)
-#    except FileNotFoundError:
-#        image_files = []
-#    
-#    image_files = sorted([foto for foto in os.listdir(assets_dir)])
-#    
-#
-#    return rx.container(
-#        rx.color_mode.button(position="top-right"),
-#        rx.grid(
-#            rx.foreach(
-#                image_files,
-#                lambda image: rx.card(
-#                    rx.image(
-#                        src=f"{image}",
-#                    #    alt=f"Imagen de {image.split('.')[0]}",
-#                        width="100%",
-#                        height="100%",
-#                        object_fit="cover",
-#                    ),
-#                ),
-#            ),
-#            columns="8",
-#            spacing="4",
-#            width="100%",
-#        ),
-#    )
-#
-#
-#app = rx.App()
-
 import reflex as rx
 import os
 
-class State(rx.State):
-    """The app state."""
 
-    ...
-@rx.page(route="/tablero", title="tablero")
-def index() -> rx.Component:
-    # Ruta de la carpeta 'assets'
-    assets_dir = os.path.join(os.path.dirname(__file__), "../assets")
-    # Obtener los nombres de los archivos de imagen
-    try:
-        image_files = sorted([f for f in os.listdir(assets_dir) if f.endswith((".png", ".jpg", ".jpeg"))])
-        print("Imágenes encontradas:", image_files)  # Agregar para depurar
-    except FileNotFoundError:
-        image_files = []  # Si la carpeta no existe, manejarlo adecuadamente.
 
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.grid(
-            rx.foreach(
-                image_files,
-                lambda image: rx.card(
-                    rx.image(
-                        src="../assets/{image}",  # Ruta actualizada a 'assets'
-                        alt=f"Imagen de {image.split('.')[0]}",
-                        width="100%",
-                        height="100%",
-                        object_fit="cover",
-                    ),
-                    rx.text(image.split(".")[0], padding="10px"),  # Nombre derivado del archivo
-                    height="20vh",
-                ),
-            ),
-            columns="8",
-            spacing="4",
-            width="100%",
-        ),
+
+
+class TableroState(rx.State):
+    img_src: str = "Alex.png"  # Initial image source
+
+    def toggle_image(self):
+        # Toggle the image source
+        self.img_src = "XAlex.png" if self.img_src == "Alex.png" else "Alex.png"
+
+def tablero():
+    return rx.grid(
+        rx.card(
+            "Alex",
+            rx.image(src=TableroState.img_src),  # Bind the image source to state
+            on_click=TableroState.toggle_image,  # Add the click event handler
+        )
     )
 
 
+#def tablero():
+#    return rx.grid(
+#        rx.card("Alex",rx.image(src=r"Alex.png")),
+#        rx.card("Alfred",rx.image(src=r"Alfred.png")),
+#        rx.card("Anita",rx.image(src=r"Anita.png")),
+#        rx.card("Anne",rx.image(src=r"Anne.png")),
+#        rx.card("Bernard",rx.image(src=r"Bernard.png")),
+#        rx.card("Bill",rx.image(src=r"Bill.png")),
+#        rx.card("Charles",rx.image(src=r"Charles.png")),
+#        rx.card("Claire",rx.image(src=r"Claire.png")),
+#        rx.card("David",rx.image(src=r"David.png")),
+#        rx.card("Eric",rx.image(src=r"Eric.png")),
+#        rx.card("Frans",rx.image(src=r"Frans.png")),
+#        rx.card("George",rx.image(src=r"George.png")),
+#        rx.card("Herman",rx.image(src=r"Herman.png")),
+#        rx.card("Joe",rx.image(src=r"Joe.png")),
+#        rx.card("Maria",rx.image(src=r"Maria.png")),
+#        rx.card("Max",rx.image(src=r"Max.png")),
+#        rx.card("Paul",rx.image(src=r"Paul.png")),
+#        rx.card("Peter",rx.image(src=r"Peter.png")),
+#        rx.card("Philip",rx.image(src=r"Philip.png")),
+#        rx.card("Richard",rx.image(src=r"Richard.png")),
+#        rx.card("Robert",rx.image(src=r"Robert.png")),
+#        rx.card("Sam",rx.image(src=r"Sam.png")),
+#        rx.card("Susan",rx.image(src=r"Susan.png")),
+#        rx.card("Tom",rx.image(src=r"Tom.png")),
+#        
+#        size="20",
+#        columns="8",
+#        spacing="4",
+#        width="100%",
+#        align = "center",
+#    )
+
+def input() -> rx.Component:
+    return rx.hstack(
+        rx.input(placeholder="Haz una pregunta"),
+        rx.button("Enviar"),
+        align = "center",
+    )
 
 
+def index() -> rx.Component:
+    return rx.container(
+        chat(),
+        action_bar(),
+    )
 
+def personaje():
+    return rx.popover.root(
+        rx.popover.trigger(
+            rx.button("Respuesta"),
+        ),
+        rx.popover.content(
+            rx.flex(
+                rx.image(src=r"Alex.png",size="20"),
+                rx.popover.close(
+                    rx.button("Close"),
+                ),
+                direction="column",
+                spacing="3",
+            ),
+        ),
+    )
 
-
-
+@rx.page(route="/", title="proyecto")
+def index() -> rx.Component:
+    return rx.container(
+        rx.vstack(
+            rx.color_mode.button(position="top-right"),
+            tablero(),
+            input(),
+            personaje(),
+        )
+    )
+    ##a##
 app = rx.App()
-app.add_page(index)
